4ff0c73addc102afb6dc683e2848b8aa41eb59cb
Andy <untalandy@gmail.com>
2021-02-12 15:34:03 -0300

    -

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-/*!
 
-=========================================================
-* Material Kit PRO React - v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
